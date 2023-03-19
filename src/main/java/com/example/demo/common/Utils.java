0d6411c1ba29a5329a1ff6b006f0d0aede78c7a1
trinh.pham <trinh.pham@onemount.com>
2023-03-19 18:02:21 +0700

    get list person

--- src/main/java/com/example/demo/common/Utils.java
+++ src/main/java/com/example/demo/common/Utils.java
@@ -3,12 +3,14 @@ package com.example.demo.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class Utils {
     public static Authentication getCurrentUser() {
@@ -185,5 +187,11 @@ public class Utils {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public static AuditorAware<String> auditorAware() {
+        // return the username of the currently authenticated user
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getName);
     }
 }
